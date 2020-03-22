8951d26e1e96afa4e5b5e3f010c86cdffce7bdd1
Nay Mello <naymellofilho@gmail.com>
2020-03-22 18:16:09 -0300

    Mudei a ordem dos escopos melhorando legibildade

--- src/server.js
+++ src/server.js
@@ -14,7 +14,7 @@ app.get('/login', function (req, res) {
         querystring.stringify({
             response_type: 'code',
             client_id: process.env.SPOTIFY_CLIENT_ID,
-            scope: 'user-read-private user-top-read user-read-email',
+            scope: 'user-read-private user-read-email user-top-read',
             redirect_uri
         }))
 })
