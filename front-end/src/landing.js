42b21bf6102a378e98c24c31241f016b100c9610
Nay Mello <naymellofilho@gmail.com>
2020-04-04 23:36:17 -0300

    Implementei inteiramente a interação mobile

--- front-end/src/landing.js
+++ front-end/src/landing.js
@@ -2,4 +2,13 @@ import './landing.scss'
 
 const navButton = document.querySelector('.hamburger')
 const [nav] = document.getElementsByTagName('nav')
+const [ul] = document.getElementsByTagName('ul')
+const [body] = document.getElementsByTagName('body')
 const logo = document.getElementById('logo')
+
+navButton.addEventListener('click', () => {
+  navButton.classList.toggle('active')
+  nav.classList.toggle('active')
+  ul.classList.toggle('active')
+  body.classList.toggle('no-scroll')
+})
