a9adc4c0f409b16803c9bc09200bdce95c2844a2
Nay Mello <naymellofilho@gmail.com>
2020-04-09 18:06:28 -0300

    Deletei funções duplicadas

--- front-end/src/functions.js
+++ front-end/src/functions.js
@@ -1,89 +1,3 @@
-let url = window.location
-let accessToken = new URLSearchParams(url.search).get('access_token')
-
-//Pega o primeiro nome do usuário na API do Spotify
-const getPersonalData = () => {
-  return fetch('https://api.spotify.com/v1/me', {
-    headers: {
-      Accept: "application/json",
-      Authorization: `Bearer ${accessToken}`,
-      "Content-Type": "application/json"
-    }
-  })
-}
-
-//Pega dados referentes a top-lists na API do Spotify
-const getTopLists = (type, timeRange, limit, offset) => {
-  return fetch(`https://api.spotify.com/v1/me/top/${type}?time_range=${timeRange}_term&limit=${limit}&offset=${offset}`, {
-    headers: {
-      Accept: "application/json",
-      Authorization: `Bearer ${accessToken}`,
-      "Content-Type": "application/json"
-    }
-  })
-}
-
-//Pega os artistas, albuns ou músicas favoritas do usuário
-export const getUserTopMusic = async (type, timeRange) => {
-  if (type !== 'albums') {
-    let res = await getTopLists(type, timeRange, 10, 0)
-    let data = await res.json()
-    return data.items
-  }
-  else {
-    //Pega albuns com duas requisições à API no endpoint de "top tracks", já que a API não apresenta um endpoint 
-    //próprio para pegar os dados de albuns favoritos
-    let res1 = await getTopLists('tracks', timeRange, 45, 0)
-    let res2 = await getTopLists('tracks', timeRange, 45, 45)
-
-    let data1 = await res1.json()
-    let data2 = await res2.json()
-
-    let tracks = data1.items.concat(data2.items); //Junta o resutado de dois fetchs de tracks em uma só array
-
-    let albums = []
-
-    for (let i in tracks) { //Coloca todos os albuns das tracks em uma array
-      albums[i] = tracks[i].album
-    }
-
-    let countedAlbums = countOccurrence(albums)
-    let sortedAlbums = sortByMostListened(countedAlbums)
-    let topAlbums = sortedAlbums.slice(0, 10)
-
-    return topAlbums
-  }
-}
-
-//Transforma de volta em uma array e organiza esta em ordem decrescente, comparando o número de ocorrência de cada album
-const sortByMostListened = (arr) => {
-  //Regra que de organização que será usada no .sort()
-  const compare = (a, b) => {
-    return b.occurrence - a.occurrence
-  }
-
-  let sortedArr = arr.sort(compare)
-
-  return sortedArr
-}
-
-//Remove objetos duplicados dentro das arrays retornadas da API do Spotify
-const countOccurrence = (arr) => {
-  let uniqueArray = []
-  let hashTable = {}
-
-  arr.forEach(item => {
-    if (!hashTable[item.id]) {
-      uniqueArray.push(hashTable[item.id] = { ...item, occurrence: 0 })
-    }
-    hashTable[item.id].occurrence++
-  });
-
-  return uniqueArray;
-}
-
-//----------------------
-
 //Mostra as imagens dos artistas, albuns ou músicas
 export const showImages = async (type, data) => {
   if (type === 'tracks') {
@@ -107,7 +21,7 @@ export const showText = async (type, timeRange, firstName, data) => {
 
   //Diminui o tamanho do título caso o nome seja grande
   if (firstName.length >= 9) {
-    document.getElementsByTagName('header')[0].style.fontSize = '18px';
+    document.getElementsByTagName('header')[0].style.fontSize = '18px'
   }
 
   const domName = document.getElementById('name')
@@ -186,13 +100,4 @@ export const setTheme = (theme) => {
 
     document.getElementById('bg-colored-logo').style.backgroundColor = textBgColor
   }
-}
-
-export const getFirstName = async () => {
-  const res = await getPersonalData()
-  const data = await res.json()
-  const displayName = data.display_name
-  const [firstName] = displayName.split(' ')
-
-  return firstName
 }
